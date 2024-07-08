e117c6d606a3eabba30ea2a3c4e9c4d6d5938302
bruno coelho <brunocoelho@luizalabs.com>
2024-07-08 17:37:32 -0300

    test_print

---
+++ aula1.py
@@ -0,0 +1,32 @@
+# PRINT
+
+print("Olé mundo e lá vamos nós!")
+print()
+print("vamos praticar!")
+print()
+
+idade= 10
+print(f"Minha idade é {idade}")
+
+print('''
+Era uma vez um coelho com um casaco azul que encantava por onde passava,
+gostava muito de brincar no bosque e sempre tinha a companhia de seu amigo Dogrei.
+
+Dogrei sempre viveu no bosque pois nao tinha um lar para onnde voltar.
+''')
+
+data_nasc = "27/08/1992"
+
+print(f"Minha idade {idade} pois sou de {data_nasc}")
+
+# MATEMATICA TESTES
+a1 = 10
+a2 = 12
+
+resp = a1 + a2
+
+print(f"variavel = {resp}")
+print()
+print("Conta direta no prin = ",a1+a2)
+print()
+
